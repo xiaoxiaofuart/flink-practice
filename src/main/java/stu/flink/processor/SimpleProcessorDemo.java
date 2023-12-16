39d7af6fbb765f029c7121f22bff2085b917154b
wujiafu <965822090@qq.com>
2023-12-16 12:25:00 +0800

    窗口join与window join

---
+++ src/main/java/stu/flink/processor/SimpleProcessorDemo.java
@@ -0,0 +1,15 @@
+package stu.flink.processor;
+
+/**
+ @BelongsProject: flink-practice
+ @BelongsPackage: stu.flink.processor
+ @Author: wujiafu
+ @CreateTime: 2023-12-05  14:43
+ @Description: TODO
+ @Version: 1.0 */
+public class SimpleProcessorDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
